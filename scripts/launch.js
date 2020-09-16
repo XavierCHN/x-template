e3d636b584bb669a929e2f81ff6b624b82f12784
Xavier Zheng <364671459@qq.com>
2020-09-16 11:54:37 +0800

    Update launch.js

--- scripts/launch.js
+++ scripts/launch.js
@@ -19,7 +19,7 @@ const { getAddonName, getDotaPath } = require('./utils');
     }
     console.log('begin to load addon=>', addon_name);
     if (addon_name == getAddonName() && map_name == null) console.log('you can use launch options\n--m map_name \n--a addon');
-    const args = ['-novid', '-tools', '-addon', '-console', addon_name];
+    const args = ['-novid', '-tools', '-addon', addon_name];
     if (map_name) args.push(`+dota_launch_custom_game ${addon_name} ${map_name}`);
     spawn(path.join(win64, 'dota2.exe'), args, { detached: true, cwd: win64 });
 })().catch((error) => {
