b3896a8b58144a0d2ee1cbda5e5e07108cb0b1d9
Xavier Zheng <Xavier@dota2x.cn>
2021-03-03 12:55:35 +0800

    use node-sass for sass compilation

--- content/panorama/webpack.config.js
+++ content/panorama/webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
                 test: /\.s[ac]ss$/,
                 loader: 'sass-loader',
                 options: {
-                    implementation: require("dart-sass"),
+                    implementation: require("node-sass"),
                     sassOptions: {
                         outputStyle: "expanded",
                     },
