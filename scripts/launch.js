b690fc7fa431a7251d19e463249d626e3044d400
XavierCHN <Xavier_CHN@live.com>
2021-07-05 20:55:01 +0800

    launch console together with dota2

--- scripts/launch.js
+++ scripts/launch.js
@@ -22,6 +22,7 @@ const { getAddonName, getDotaPath } = require('./utils');
     const args = ['-novid', '-tools', '-addon', addon_name];
     if (map_name) args.push(`+dota_launch_custom_game ${addon_name} ${map_name}`);
     spawn(path.join(win64, 'dota2.exe'), args, { detached: true, cwd: win64 });
+    spawn(path.join(win64, 'vconsole2.exe'));
 })().catch((error) => {
     console.error(error);
     process.exit(1);
