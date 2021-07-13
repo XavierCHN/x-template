ae4565bae9b406cf4b760fc338dd5e74f9aebadc
XavierCHN <Xavier_CHN@live.com>
2021-07-13 21:19:26 +0800

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,32 +1,25 @@
-const { findSteamAppByName, SteamNotFoundError } = require('find-steam-app');
-const packageJson = require('../package.json');
-const fs = require('fs-extra');
-const { getGamePath } = require('steam-game-path');
+const packageJson = require("../package.json");
+const fs = require("fs-extra");
+const { getGamePath } = require("steam-game-path");
 
 module.exports.getAddonName = () => {
     if (!/^[a-z]([\d_a-z]+)?$/.test(packageJson.name)) {
-        throw new Error('Addon name may consist only of lowercase characters, digits, and underscores ' + 'and should start with a letter. Edit `name` field in `package.json` file.');
+        throw new Error("Addon name may consist only of lowercase characters, digits, and underscores " + "and should start with a letter. Edit `name` field in `package.json` file.");
     }
 
     return packageJson.name;
 };
 
 module.exports.getDotaPath = async () => {
-    try {
-        const path = getGamePath(570);
-        return path.game.path;
-    } catch (error) {
-        if (!(error instanceof SteamNotFoundError)) {
-            throw error;
-        }
-    }
+    const path = getGamePath(570);
+    return path.game.path;
 };
 
 read_all_files = (path) => {
     var pa = fs.readdirSync(path);
     var files = [];
     pa.forEach((ele, index) => {
-        let child = path + '/' + ele;
+        let child = path + "/" + ele;
         let info = fs.statSync(child);
         if (info.isDirectory()) {
             let subs = read_all_files(child);
@@ -43,7 +36,7 @@ read_sub_directories = (path) => {
     var pa = fs.readdirSync(path);
     var directories = [];
     pa.forEach((ele, index) => {
-        let child = path + '/' + ele;
+        let child = path + "/" + ele;
         let info = fs.statSync(child);
         if (info.isDirectory()) {
             directories.push(child);
