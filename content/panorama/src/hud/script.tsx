396f9c23dd4576a6a92cc91efba0572a4df0e076
XavierCHN <Xavier_CHN@live.com>
2022-03-02 18:48:26 +0800

    remove the useless react template

--- content/panorama/src/hud/script.tsx
+++ content/panorama/src/hud/script.tsx
@@ -1,10 +1,4 @@
-import React from 'react';
-import { render } from '@demon673/react-panorama';
-import { ReactLogo } from './components/react_panorama';
+// import React from 'react';
+// import { render } from '@demon673/react-panorama';
 
-render(<ReactLogo />, $.GetContextPanel()); // 默认在中间渲染的红色REACT-PANORAMA标志，从这里开始修改为你自己喜欢的
-
-(() => {
-    // GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_HEROES, false); // 你也可以按你之前喜欢的方式写代码
-    // GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_TIMEOFDAY, false);
-})();
+// render(<ReactPanoramaPanel />, $.GetContextPanel()); // an example of how to render a panel
