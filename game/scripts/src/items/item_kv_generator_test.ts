657592dc772a63b2de15bf5c9b6c8c931d3b1bf6
Xavier Zheng <Xavier@dota2x.cn>
2022-03-09 11:40:21 +0800

    removed test lines

--- game/scripts/src/items/item_kv_generator_test.ts
+++
@@ -1,9 +0,0 @@
-import { BaseItem, registerAbility } from "../lib/dota_ts_adapter";
-
-@registerAbility()
-export class item_kv_generator_test extends BaseItem {
-    OnEquip(): void {
-        let damage = this.GetSpecialValueFor("damage");
-        print(damage);
-    }
-}
