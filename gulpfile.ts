318d8b17be5716c187590b7ede0a3c9db66f444f
Xavier Zheng <Xavier_CHN@Live.com>
2022-06-20 15:55:48 +0800

    update to gulp version

---
+++ gulpfile.ts
@@ -0,0 +1,139 @@
+import gulp from "gulp";
+import * as dotax from "gulp-dotax";
+import path from "path";
+
+const paths: { [key: string]: string; } = {
+    excels: "excels",
+    kv: "game/scripts/npc",
+    panorama: "content/panorama",
+    game_resource: "game/resource",
+};
+
+/**
+ * @description 将excel文件转换为kv文件
+ * @description Convert your excel file to kv file
+ */
+const cSheetToKV = async () => {
+    gulp.src(`${paths.excels}/*.{xlsx,xls}`)
+        .pipe(
+            dotax.sheetToKV({
+                sheetsIgnore: "^__", // 忽略以两个下划线开头的sheet
+                indent: `	`, // 自定义缩进
+            })
+        )
+        .pipe(gulp.dest(paths.kv));
+};
+
+/**
+ * @description 将kv文件转换为panorama使用的js文件，你需要在webpack.config.js中配置相应的loader
+ * @description Convert your kv file to panorama js file, you need to configure the loader in webpack.config.js
+ */
+const cKVToJS = async () => {
+    gulp.src(`${paths.kv}/**/*.{kv,txt}`)
+        .pipe(
+            dotax.kvToJS({
+                fileName: "sync_keyvalues.js",
+            })
+        )
+        .pipe(gulp.dest(`${paths.panorama}/src/utils`));
+};
+
+/**
+ * @description 从 kv 文件中提取所有的description，你可以使用 customPrefix 和 customSuffix 之类的参数来指定自己的前缀和后缀
+ * @description Extract all description from kv file, you can use customPrefix and customSuffix to specify your prefix and suffix
+ */
+const cKVToLocal = async () => {
+    gulp.src(`${paths.kv}/**/*.{kv,txt}`).pipe(
+        dotax.kvToLocalsCSV(`${paths.game_resource}/addon.csv`, {
+            customPrefix: (key, data, path) => {
+                if (data.BaseClass && /ability_/.test(data.BaseClass)) {
+                    if (data.ScriptFile && data.ScriptFile.startsWith("abilities/combos/")) {
+                        return "dota_tooltip_ability_combo_";
+                    } else if (data.ScriptFile && /^/.test(data.ScriptFile)) {
+                        return "dota_tooltip_ability_chess_ability_";
+                    } else {
+                        return "dota_tooltip_ability_";
+                    }
+                }
+                return "";
+            },
+            customSuffix: (key, data, path) => {
+                let suffix = [""];
+                if (data.ScriptFile && data.ScriptFile.startsWith("abilities/combos/")) {
+                    suffix = ["_description"];
+                    let maxLevel = data.MaxLevel;
+                    if (maxLevel) {
+                        suffix = suffix.concat(Array.from({ length: maxLevel }, (_, i) => `_level${i + 1}`));
+                    }
+                }
+                return suffix;
+            },
+            exportAbilityValues: false,
+        })
+    );
+};
+
+/**
+ * @description 将 addon.csv 中的本地化文本转换为 addon_*.txt 文件，这个 task 建议 kv 写完了之后再运行一次即可
+ * @description Convert addon.csv local text to addon_*.txt file， this task is recommended to run after kv is finished
+ *
+ */
+const cCSVToLocalization = async () => {
+    gulp.src(`${paths.game_resource}/addon.csv`).pipe(dotax.csvToLocals(paths.game_resource));
+};
+
+/**
+ * @description 将现有的 addon_*.txt 文件转换为 addon.csv 文件，这个 task 是为了使这个task适配你原有的开发方式，如果是重新开发，则无需运行这个task
+ * @description Convert addon_*.txt file to addon.csv file, this task is for adapting your original development method, if you are re-developing, you don't need to run this task
+ */
+const cLocalsToCSV = async () => {
+    dotax.localsToCSV(`${paths.game_resource}/addon_*.txt`, `${paths.game_resource}/addon.csv`);
+};
+
+/**
+ * 将panorama/images目录下的jpg,png,psd文件集合到 dest 目录中的 image_precache.css文件中
+ * 使用这个 task ，你可以在 game setup 阶段的时候，将所有的图片都编译而不用自己写
+ */
+gulp.task(`img_pcache`, async () => {
+    await gulp
+        .src(`content/panorama/images/**/*.{jpg,png,psd}`)
+        .pipe(dotax.imagePrecacche(`content/panorama/images/`))
+        .pipe(gulp.dest(path.join(paths.panorama, `src/utils`)));
+});
+gulp.task("img_pcache:watch", async () => {
+    gulp.watch(`content/panorama/images/**/*.{jpg,png,psd}`, gulp.series("img_pcache"));
+});
+
+// 以下的task，顾名思义即可
+gulp.task("local", cLocalsToCSV);
+
+gulp.task("sheetToKV", cSheetToKV);
+gulp.task("sheetToKV:watch", () => {
+    gulp.watch(`${paths.excels}/*.{xlsx,xls,csv}`, cSheetToKV);
+});
+
+gulp.task("kvToJS", cKVToJS);
+gulp.task("kvToJS:watch", () => {
+    gulp.watch(`${paths.kv}/**/*.{kv,txt}`, cKVToJS);
+});
+
+gulp.task("kvToLocal", cKVToLocal);
+gulp.task("kvToLocal:watch", (callback: Function) => {
+    gulp.watch(`${paths.kv}/**/*.{kv,txt}`, cKVToLocal);
+    callback();
+});
+
+gulp.task("csvToLocalization", cCSVToLocalization);
+gulp.task("csvToLocalization:watch", (callback: Function) => {
+    gulp.watch(`${paths.game_resource}/addon.csv`, cCSVToLocalization);
+    callback();
+});
+
+gulp.task("predev", gulp.series("sheetToKV", "kvToJS", "csvToLocalization", "img_pcache"));
+gulp.task("dev", gulp.parallel("sheetToKV:watch", "csvToLocalization:watch", "img_pcache:watch"));
+gulp.task("build", gulp.series("predev", "csvToLocalization"));
+gulp.task("jssync", gulp.series("sheetToKV", "kvToJS"));
+gulp.task("kvl", gulp.series("kvToLocal"));
+
+// 编译发布版本
+gulp.task("prod", gulp.series("sheetToKV", "kvToJS", "csvToLocalization", "img_pcache"));
--- scripts/publish.js
+++ scripts/publish.js
@@ -10,32 +10,44 @@ const settings = packageJson.dota_developer.publish_options;
 const walker = walk.walk("game");
 const excludeFiles = settings.excludeFiles;
 const encryptFiles = settings.encryptFiles;
-const dedicatedServerKey = settings.encryptDedicatedServerKey;
+
+let mode = process.argv[2];
+const dedicatedServerKey = mode == `release` ? settings.encryptDedicatedServerKeyRelease : settings.encryptDedicatedServerKeyTest;
 const exec = require("child_process").exec;
 
 const getPublishPath = (source) => source.replace(/^game/, "publish");
 
-walker.on("file", (root, fileStats, next) => {
-    const fileName = path.join(root, fileStats.name);
-    if (anyMatch(excludeFiles, fileName)) {
-        // ignore the files we dont want to publish
-        console.log(`[publish.js] ignore filtered file ->${fileName}`);
-    } else {
-        if (!fs.existsSync(getPublishPath(root))) {
-            fs.mkdirSync(getPublishPath(root), { recursive: true });
-            console.log(`[publish.js] [create-path] ->${root}`);
-        }
-        if (anyMatch(encryptFiles, fileName)) {
-            exec(`lua scripts/encrypt_file.lua "${fileName}" "${getPublishPath(fileName)}" ${dedicatedServerKey}`, (err, out) => {
-                if (err) console.error(`[publish.js] [encrypt] ->${fileName}`, err);
-                if (!err) {
-                    console.log(`[publish.js] [encrypt] ->${fileName} successed with key ${dedicatedServerKey}`);
-                }
-            });
+let encryptCount = 0;
+walker
+    .on("file", (root, fileStats, next) => {
+        const fileName = path.join(root, fileStats.name);
+        if (anyMatch(excludeFiles, fileName)) {
+            // ignore the files we dont want to publish
+            console.log(`[publish.js] ignore filtered file ->${fileName}`);
         } else {
-            console.log(`[publish.js] [copy] ->${fileName}`);
-            fs.copyFileSync(fileName, getPublishPath(fileName));
+            if (!fs.existsSync(getPublishPath(root))) {
+                fs.mkdirSync(getPublishPath(root), { recursive: true });
+                console.log(`[publish.js] [create-path] ->${root}`);
+            }
+            if (anyMatch(encryptFiles, fileName)) {
+                exec(`lua scripts/encrypt_file.lua "${fileName}" "${getPublishPath(fileName)}" ${dedicatedServerKey}`, (err, out) => {
+                    if (err) console.error(`[publish.js] [encrypt] ->${fileName}`, err);
+                    if (!err) {
+                        encryptCount++;
+                        console.log(`[publish.js] [encrypt] ->${fileName} successed with key ${dedicatedServerKey}`);
+                    }
+                });
+            } else {
+                console.log(`[publish.js] [copy] ->${fileName}`);
+                fs.copyFileSync(fileName, getPublishPath(fileName));
+            }
         }
-    }
-    next();
-});
+        next();
+    })
+    .on(`end`, () => {
+        console.log(
+            `发布完成，发布模式是 ${
+                mode == `release` ? `正式发布` : `测试发布`
+            }\n其中${encryptCount}个Lua文件使用 ${dedicatedServerKey} 加密!\n正在启动dota2，\n如果是测试发布请查看游戏运行是否正常！如果是正式发布请直接上传`
+        );
+    });
