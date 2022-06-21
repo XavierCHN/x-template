06c007a0abe1dec47690ebeb6afa39635288b121
Xavier Zheng <Xavier_CHN@Live.com>
2022-06-21 10:16:56 +0800

    oops

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,6 +2,14 @@ const packageJson = require("../package.json");
 const fs = require("fs-extra");
 const { getGamePath } = require("steam-game-path");
 
+module.exports.getAddonName = () => {
+    if (!/^[a-z]([\d_a-z]+)?$/.test(packageJson.name)) {
+        throw new Error("Addon name may consist only of lowercase characters, digits, and underscores " + "and should start with a letter. Edit `name` field in `package.json` file.");
+    }
+
+    return packageJson.name;
+};
+
 module.exports.getDotaPath = async () => {
     const path = getGamePath(570);
     return path.game.path;
