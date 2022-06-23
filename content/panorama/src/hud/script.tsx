b449d3c8b78e326ab7116050d0763daafbe032d8
Xavier Zheng <Xavier_CHN@live.com>
2022-06-23 19:23:20 +0800

    Update script.tsx

--- content/panorama/src/hud/script.tsx
+++ content/panorama/src/hud/script.tsx
@@ -1,4 +1,2 @@
-// import React from 'react';
-// import { render } from '@demon673/react-panorama';
-
-// render(<ReactPanoramaPanel />, $.GetContextPanel()); // an example of how to render a panel
+import React from 'react';
+import { render } from '@demon673/react-panorama';
