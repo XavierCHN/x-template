2412487f8ce6b4d6763517c5aed1a0f790d5c338
Xavier Zheng <Xavier_CHN@Live.com>
2022-08-26 15:22:43 +0800

    clear scripts

--- content/panorama/src/def/local_event_def.ts
+++ content/panorama/src/def/local_event_def.ts
@@ -4,12 +4,6 @@
  * @interface LocalEvent
  */
 export interface LocalEvent {
-
-    // 网表数据广播
-    data_hub_updated: {
-        type: string;
-    };
-
     // 收到网表更新事件
     x_net_table: {
         table_name: string;
