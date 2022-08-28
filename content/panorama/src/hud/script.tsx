f497c8a51c2ba71f741cc617d108dad3fb028850
Xavier Zheng <Xavier_CHN@live.com>
2022-08-28 17:11:30 +0800

    add the place holder for react-panorama

--- content/panorama/src/hud/script.tsx
+++ content/panorama/src/hud/script.tsx
@@ -1,2 +1,11 @@
 import "panorama-polyfill/lib/console";
 import "panorama-polyfill/lib/timers";
+
+import React from "react";
+import { render } from "@demon673/react-panorama";
+
+function Test() {
+    return <Label text="this is a placeholder for react-panorama" />;
+}
+
+render(<Test />, $.GetContextPanel());
