7f4154fd0ea43f4b487530a94ac34136d86af456
Xavier Zheng <Xavier_CHN@Live.com>
2022-08-29 11:13:18 +0800

    Update launch.js for yarn

--- scripts/launch.js
+++ scripts/launch.js
@@ -1,6 +1,6 @@
-const { spawn } = require("child_process");
-const path = require("path");
-const { getAddonName, getDotaPath } = require("./utils");
+const { spawn } = require('child_process');
+const path = require('path');
+const { getAddonName, getDotaPath } = require('./utils');
 
 (async () => {
     const dotaPath = await getDotaPath();
@@ -10,10 +10,8 @@ const { getAddonName, getDotaPath } = require("./utils");
         return;
     }
 
-    const win64 = path.join(dotaPath, "game", "bin", "win64");
+    const win64 = path.join(dotaPath, 'game', 'bin', 'win64');
 
-    // You can add any arguments there
-    // For example `+dota_launch_custom_game ${getAddonName()} dota` would automatically load "dota" map
     let addon_name = getAddonName();
     let map_name;
 
@@ -21,23 +19,46 @@ const { getAddonName, getDotaPath } = require("./utils");
     let params = script.match(/"(?:\\?[\S\s])*?"/g);
     if (params != null && params.length > 0) {
         if (params.length == 1) {
-            map_name = params[0].replace(/"/g, "");
+            map_name = params[0].replace(/"/g, '');
         } else {
-            addon_name = params[0].replace(/"/g, "");
-            map_name = params[1].replace(/"/g, "");
+            addon_name = params[0].replace(/"/g, '');
+            map_name = params[1].replace(/"/g, '');
+        }
+    }
+
+    if (process.env.npm_config_argv != null) {
+        let argv = JSON.parse(process.env.npm_config_argv);
+        if (argv.original != null && argv.original.length > 0) {
+            let args = argv.original.slice(1);
+            if (args[0] == `launch`) { // 如果有手动输入run的情况
+                args = args.slice(1);
+            }
+            if (args.length > 0) {
+                if (args.length == 1) {
+                    map_name = args[0];
+                }
+                if (args.length >= 2) {
+                    addon_name = args[0];
+                    map_name = args[1];
+                }
+            }
         }
     }
 
     if (map_name != null) console.log(`begin to load addon=>${addon_name}, map name=>${map_name}`);
-    else console.log("you can use launch with `npm run launch [[addon_name] map_name]");
+    else console.log('Usage `yarn launch [[addon name] map name]`');
 
-    const args = ["-novid", "-tools", "-addon", addon_name];
+    const args = ['-novid', '-tools', '-addon', addon_name];
     if (map_name) args.push(`+dota_launch_custom_game ${addon_name} ${map_name}`);
 
-    if (map_name == `dotaonly`) spawn(path.join(win64, "dota2.exe"), [`-novid`], { detached: true, cwd: win64 });
+    if (map_name == `dotaonly`)
+        spawn(path.join(win64, 'dota2.exe'), [`-novid`, '-perfectworld'], {
+            detached: true,
+            cwd: win64,
+        });
     else {
-        spawn(path.join(win64, "dota2.exe"), args, { detached: true, cwd: win64 });
-        spawn(path.join(win64, "vconsole2.exe"));
+        spawn(path.join(win64, 'dota2.exe'), args, { detached: true, cwd: win64 });
+        spawn(path.join(win64, 'vconsole2.exe'));
     }
 })().catch((error) => {
     console.error(error);
