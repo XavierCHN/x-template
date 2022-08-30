e37f04c37e3529f34478b017eeea55f5b0d7fbaf
Xavier Zheng <Xavier_CHN@live.com>
2022-08-30 19:29:06 +0800

    Update clean_api.js

--- scripts/clean_api.js
+++ scripts/clean_api.js
@@ -6,7 +6,9 @@ fs.removeSync(`game/scripts/src/server/index.ts`);
 
 // 修改GameService里面的内容
 const gameService = fs.readFileSync(`game/scripts/src/server/services/GameService.ts`, `utf8`);
-const gameServiceNew = gameService.replace(`import type { CancelablePromise } from '../core/CancelablePromise';`, `\n`).replace(/CancelablePromise/g, `Promise`);
+const gameServiceNew = gameService
+    .replace(`import type { CancelablePromise } from '../core/CancelablePromise';`, `\n`)
+    .replace(/CancelablePromise/g, `Promise`);
 
 // 输出所有API作为types
 const apis = gameServiceNew.match(/url:\s*(['"][a-zA-Z0-9\/]+['"])/g);
@@ -16,7 +18,10 @@ const api_type_string = apis
     })
     .sort()
     .join(` | \n`);
-fs.writeFileSync(`game/scripts/src/server/core/urls.ts`, `export type URLs = \n${api_type_string};\n`);
+fs.writeFileSync(
+    `game/scripts/src/server/core/urls.ts`,
+    `export type URLs = \n${api_type_string};\n`
+);
 
 // 重新写入文件
 fs.writeFileSync(`game/scripts/src/server/services/GameService.ts`, gameServiceNew);
