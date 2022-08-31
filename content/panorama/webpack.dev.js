7466f24d615e87471cd0e47ebdd3b851e0275be8
Xavier Zheng <Xavier_CHN@live.com>
2022-08-31 11:17:59 +0800

    Update webpack.dev.js

--- content/panorama/webpack.dev.js
+++ content/panorama/webpack.dev.js
@@ -1,19 +1,19 @@
-const path = require("path");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const { PanoramaManifestPlugin, PanoramaTargetPlugin } = require("@demon673/webpack-panorama");
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { PanoramaManifestPlugin, PanoramaTargetPlugin } = require('@demon673/webpack-panorama');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-    mode: "development",
-    context: path.resolve(__dirname, "src"),
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
     output: {
-        path: path.resolve(__dirname, "layout/custom_game"),
-        publicPath: "file://{resources}/layout/custom_game/",
-        chunkFormat: "commonjs"
+        path: path.resolve(__dirname, 'layout/custom_game'),
+        publicPath: 'file://{resources}/layout/custom_game/',
+        chunkFormat: 'commonjs',
     },
 
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx", "..."],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '...'],
         symlinks: false,
     },
 
@@ -21,42 +21,44 @@ module.exports = {
         rules: [
             {
                 test: /\.xml$/,
-                loader: "@demon673/webpack-panorama/lib/layout-loader",
-                options: {
-                },
+                loader: '@demon673/webpack-panorama/lib/layout-loader',
+                options: {},
             },
             {
                 test: /\.[jt]sx$/,
                 issuer: /\.xml$/,
-                loader: "@demon673/webpack-panorama/lib/entry-loader",
+                loader: '@demon673/webpack-panorama/lib/entry-loader',
             },
             {
                 test: /\.tsx?$/,
-                loader: "ts-loader",
+                loader: 'ts-loader',
                 options: { transpileOnly: true },
             },
             {
                 test: /\.js?$|\.jsx?$/,
-                loader: "babel-loader",
+                loader: 'babel-loader',
                 exclude: [/node_modules/],
-                options: { presets: ["@babel/preset-react", "@babel/preset-env"] },
+                options: { presets: ['@babel/preset-react', '@babel/preset-env'] },
             },
             {
                 test: /\.css$/,
                 test: /\.(css|less)$/,
                 issuer: /\.xml$/,
-                loader: "file-loader",
-                options: { name: "[path][name].css", esModule: false },
+                loader: 'file-loader',
+                options: { name: '[path][name].css', esModule: false },
             },
             {
                 test: /\.less$/,
-                loader: "less-loader",
+                loader: 'less-loader',
                 options: {
                     additionalData: (content) => {
-                        content = content.replace(/@keyframes\s*(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)/g, (match, name) => {
-                            // add apostrophe to satisfy valve
-                            return match.replace(name, `'${name}'`);
-                        });
+                        content = content.replace(
+                            /@keyframes\s*(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)/g,
+                            (match, name) => {
+                                // add apostrophe to satisfy valve
+                                return match.replace(name, `'${name}'`);
+                            }
+                        );
                         return content;
                     },
                     lessOptions: {
@@ -72,18 +74,18 @@ module.exports = {
         new PanoramaManifestPlugin({
             entries: [
                 {
-                    import: "./loading-screen/layout.xml",
-                    filename: "custom_loading_screen.xml",
+                    import: './loading-screen/layout.xml',
+                    filename: 'custom_loading_screen.xml',
                 },
-                { import: "./hud/layout.xml", type: "Hud" },
-                { import: "./end_screen/layout.xml", type: "EndScreen" },
+                { import: './hud/layout.xml', type: 'Hud' },
+                { import: './end_screen/layout.xml', type: 'EndScreen' },
             ],
             // 这是一个临时的解决方案，应该作为一个永久性的变更放到webpack-panorama中
             minify: false,
         }),
         new ForkTsCheckerWebpackPlugin({
             typescript: {
-                configFile: path.resolve(__dirname, "tsconfig.json"),
+                configFile: path.resolve(__dirname, 'tsconfig.json'),
             },
         }),
     ],
