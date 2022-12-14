653d2a16c58b6874c34cce4adc536eaa34c6ce37
Xavier Zheng <Xavier_CHN@live.com>
2022-12-14 13:14:58 +0800

    加入`Precache.ts`文件的范例
    Update precache.ts
    修正tstl导致的一个错误

--- game/scripts/src/utils/precache.ts
+++ game/scripts/src/utils/precache.ts
@@ -1,11 +1,95 @@
-function precacheEveryResourceInKV(kvFileList: string[]) {}
-function precacheResource(resourceList: string[]) {}
-function precacheUnits(unitNamesList: string[]) {}
+/** @noSelfInFile */
+// 导出的预载入方法，用来给addon_game_mode.ts调用
 export default function Precache(context: CScriptPrecacheContext) {
-    precacheResource([
-        // 需要预载的所有资源
-        '***.vpcf',
-        '***.vsndevts',
-        '***.vmdl',
-    ]);
+    // 需要预载的所有资源
+    precacheResource(
+        [
+            // '***.vpcf',
+            // 'soundevents/game_sounds_heroes/game_sounds_queenofpain.vsndevts',
+            // '***.vmdl',
+        ],
+        context
+    );
+    // 需要预载入的kv文件，会自动解析KV文件中的所有vpcf资源等等
+    precacheEveryResourceInKV(
+        [
+            // kv文件路径
+            // 'npc_abilities_custom.txt',
+        ],
+        context
+    );
+    // 需要预载入的单位
+    precacheUnits(
+        [
+            // 单位名称
+            // 'npc_dota_hero_***',
+        ],
+        context
+    );
+    // 需要预载入的物品
+    precacheItems(
+        [
+            // 物品名称
+            // 'item_***',
+        ],
+        context
+    );
+    print(`[Precache] Precache finished.`);
+}
+
+// 预载入KV文件中的所有资源
+function precacheEveryResourceInKV(kvFileList: string[], context: CScriptPrecacheContext) {
+    kvFileList.forEach(file => {
+        const kvTable = LoadKeyValues(file);
+        precacheEverythingFromTable(kvTable, context);
+    });
+}
+// 预载入资源列表
+function precacheResource(resourceList: string[], context: CScriptPrecacheContext) {
+    resourceList.forEach(resource => {
+        const res = resource as string;
+        if (res.endsWith('.vpcf')) {
+            PrecacheResource('particle', res, context);
+        } else if (res.endsWith('.vsndevts')) {
+            PrecacheResource('soundfile', res, context);
+        } else if (res.endsWith('.vmdl')) {
+            PrecacheResource('model', res, context);
+        }
+    });
+}
+// 预载入单位列表
+function precacheUnits(unitNamesList: string[], context?: CScriptPrecacheContext) {
+    if (context != null) {
+        unitNamesList.forEach(unitName => {
+            PrecacheUnitByNameSync(unitName, context);
+        });
+    } else {
+        unitNamesList.forEach(unitName => {
+            PrecacheUnitByNameAsync(unitName, () => {});
+        });
+    }
+}
+// 预载入物品列表
+function precacheItems(itemList: string[], context: CScriptPrecacheContext) {
+    itemList.forEach(itemName => {
+        PrecacheItemByNameSync(itemName, context);
+    });
+}
+
+// 一个辅助的，从KV表中解析出所有资源并预载入的方法
+function precacheEverythingFromTable(kvTable: any, context: CScriptPrecacheContext) {
+    for (const [k, v] of pairs(kvTable)) {
+        if (type(v) === 'table') {
+            precacheEverythingFromTable(v, context);
+        } else if (type(v) === 'string') {
+            const res = v as string;
+            if (res.endsWith('.vpcf')) {
+                PrecacheResource('particle', res, context);
+            } else if (res.endsWith('.vsndevts')) {
+                PrecacheResource('soundfile', res, context);
+            } else if (res.endsWith('.vmdl')) {
+                PrecacheResource('model', res, context);
+            }
+        }
+    }
 }
