9024d895be55cd3a4e7f304a27cf03f52991f5be
Xavier Zheng <Xavier_CHN@Live.com>
2023-01-31 14:51:53 +0800

    always use type import rule for better performance

--- content/panorama/src/hud/script.tsx
+++ content/panorama/src/hud/script.tsx
@@ -1,11 +1,12 @@
 import 'panorama-polyfill-x/lib/console';
 import 'panorama-polyfill-x/lib/timers';
 
+import type { FC } from 'react';
 import React from 'react';
 import { render } from 'react-panorama-x';
 import { useXNetTableKey } from '../hooks/useXNetTable';
 
-const Test: React.FC = () => {
+const Test: FC = () => {
     const [data] = useXNetTableKey(`test_table`, `test_key`, { data_1: `unknown` });
     const string_data = data.data_1;
     return React.useMemo(() => <Label text={`${string_data}`} />, [string_data]);
