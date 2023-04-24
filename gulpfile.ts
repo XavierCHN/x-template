b9064e40fea81dde887ef07af13f6e587c7199bd
Xavier Zheng <Xavier_CHN@Live.com>
2023-04-24 11:28:08 +0800

    将kv生成的本地化文本和其他部分独立开，同时也作为能提供多csv支持的范例

--- gulpfile.ts
+++ gulpfile.ts
@@ -27,7 +27,7 @@ const sheet_2_kv =
                         // 所有支持的参数请按住 Ctrl 点击 sheetToKV 查看，以下其他 API 也是如此
                         sheetsIgnore: '^__.*|^Sheet[1-3]$', // 忽略以两个下划线开头的sheet 和 默认生成的 Sheet1 Sheet2 Sheet3 等
                         indent: `	`, // 自定义缩进
-                        addonCSVPath: `${paths.game_resource}/addon.csv`, // 本地化文件路径，用以将 excel 文件中的 #Loc{}输出到addon.csv文件中去
+                        addonCSVPath: `${paths.game_resource}/kv_generated.csv`, // 本地化文件路径，用以将 excel 文件中的 #Loc{}输出到addon.csv文件中去
                     })
                 )
                 .pipe(gulp.dest(paths.kv));
