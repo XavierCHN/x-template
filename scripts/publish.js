3771d1dea21e4b96f149afa6bb4306961c02d1a3
Xavier Zheng <Xavier_CHN@live.com>
2023-06-11 00:30:10 +0800

    show file stats

--- scripts/publish.js
+++ scripts/publish.js
@@ -26,13 +26,14 @@ const exec = require('child_process').exec;
 
 const getPublishPath = source => source.replace(/^game/, 'publish');
 
-let encryptCount = 0;
+let stats = {}; // stastics file count of copy, ignore and encrypted
 walker
     .on('file', (root, fileStats, next) => {
         const fileName = path.join(root, fileStats.name);
         if (anyMatch(excludeFiles, fileName)) {
             // ignore the files we dont want to publish
             console.log(`[publish.js] ignore filtered file ->${fileName}`);
+            stats.ignore = stats.ignore ? stats.ignore + 1 : 1;
         } else {
             if (!fs.existsSync(getPublishPath(root))) {
                 fs.mkdirSync(getPublishPath(root), { recursive: true });
@@ -42,13 +43,14 @@ walker
                 exec(`lua scripts/encrypt_file.lua "${fileName}" "${getPublishPath(fileName)}" ${dedicatedServerKey}`, (err, out) => {
                     if (err) console.error(`[publish.js] [encrypt] ->${fileName}`, err);
                     if (!err) {
-                        encryptCount++;
+                        stats.encrypted = stats.encrypted ? stats.encrypted + 1 : 1;
                         console.log(`[publish.js] [encrypt] ->${fileName} successed with key ${dedicatedServerKey}`);
                     }
                 });
             } else {
                 console.log(`[publish.js] [copy] ->${fileName}`);
                 fs.copyFileSync(fileName, getPublishPath(fileName));
+                stats.copy = stats.copy ? stats.copy + 1 : 1;
             }
             if (/addon_game_mode\.lua$/.test(fileName)) {
                 const addonGameMode = fs.readFileSync(getPublishPath(fileName), 'utf8');
@@ -68,12 +70,12 @@ walker
     })
     .on(`end`, async () => {
         console.log(
-            `发布完成，发布模式是 ${color.red(
-                mode == `release` ? `正式发布` : mode == `release_test` ? `在线测试` : `本地测试`
-            )}\n其中${encryptCount}个Lua文件使用 ${color.red(
-                dedicatedServerKey
-            )} 加密!\n正在启动dota2，\n如果是测试发布请查看游戏运行是否正常！如果是正式发布请直接上传`
+            `发布完成，发布模式是 ${color.red(mode == `release` ? `正式发布` : mode == `release_test` ? `在线测试` : `本地测试`)}\n其中${
+                stats.encrypted ?? 0
+            }个Lua文件使用 ${color.red(dedicatedServerKey)} 加密!\n正在启动dota2，\n如果是测试发布请查看游戏运行是否正常！如果是正式发布请直接上传`
         );
+        console.log(`[文件数量统计] -> ${JSON.stringify(stats, null, 4)}`);
+
         const addon_name = config.addon_name;
 
         if (mode != `release` && mode != `release_test`) {
