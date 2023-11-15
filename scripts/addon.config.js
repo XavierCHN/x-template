285dda0ffe36d2ba2a573687c980204a7b085dc9
Xavier(Home) <Xavier_CHN@live.com>
2023-11-15 10:08:11 +0800

    Update addon.config.js

--- scripts/addon.config.js
+++ scripts/addon.config.js
@@ -1,5 +1,5 @@
 /** 项目名称 */
-let addon_name = 'x_template'; // 必须为字母开头，而且只能包含字母、数字和下划线
+let addon_name = 'x_template'; // 必须为字母开头，而且只能包含小写字母、数字和下划线
 
 try {
     addon_name = require('./.test/addon_name');
