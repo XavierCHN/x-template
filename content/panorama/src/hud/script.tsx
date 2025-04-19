3577b666554fd47056afd85e7f5e77a4991b7241
Xavier(Home) <Xavier_CHN@live.com>
2025-04-19 19:10:22 +0800

    不再默认渲染火焰图

--- content/panorama/src/hud/script.tsx
+++ content/panorama/src/hud/script.tsx
@@ -1,21 +1,13 @@
 import 'panorama-polyfill-x/lib/console';
 import 'panorama-polyfill-x/lib/timers';
-import { FlameGraph } from '../utils/flame_graph/flame_graph';
 
 import { type FC } from 'react';
 import { render } from 'react-panorama-x';
 import { PanoramaQRCode } from '../utils/react-panorama-qrcode';
-import { useXNetTableKey } from '../hooks/useXNetTable';
 
 const Root: FC = () => {
-    const data = useXNetTableKey(`test_table`, `test_key`, { data_1: 'unknown' });
-    console.log(data);
     return (
         <>
-            {
-                // 如果在工具模式下显示火焰图
-                Game.IsInToolsMode() && <FlameGraph />
-            }
             {
                 // 显示一个二维码作为范例
                 <PanoramaQRCode
