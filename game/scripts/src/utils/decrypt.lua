c4482585371ef7919943d10a1380fbe1c5a282db
XavierCHN <Xavier_CHN@Live.com>
2025-04-23 09:52:36 +0800

    加入一个双端代码加密运行时的错误提示帮助用户了解到底是哪个文件有问题

--- game/scripts/src/utils/decrypt.lua
+++ game/scripts/src/utils/decrypt.lua
@@ -1,6 +1,3 @@
--- 目前这个解密模块只在服务器端运行
-if not IsServer() then return end
-
 function string.fromhex(str)
     return (str:gsub(
         "..",
@@ -27,10 +24,19 @@ end
 -- https://github.com/XavierCHN/x-template/blob/master/scripts/addon.config.ts#L39-L40
 -- 关于他的获取方式请参考此项目
 -- https://github.com/XavierCHN/fetch-keys
-local key = GetDedicatedServerKeyV3('version')
-
-GameRules.XDecrypt = function(code, ...)
-    local text = string.fromhex(code)
-    local plain = aeslua.decrypt(key, text, aeslua.AES128, aeslua.CBCMODE)
-    return loadstring(plain)(...)
+if not IsServer() then 
+    -- 如果客户端尝试解密，那么输出一个报错
+    -- 当然，你这里也可以将这个方法修改为“不安全的做法”
+    -- 请自行深入研究以避免暴露密钥（自己找一个“藏密钥”的方法吧~）
+    GameRules.XDecrypt = function(code, ...)
+        error(debug.traceback('请不要加密此文件的代码，因为客户端没有安全可用的密钥\n请到addon.config.ts中将此文件排除加密'))
+    end
+else
+    -- 服务器端的解密方法    
+    local key = GetDedicatedServerKeyV3('version')
+    GameRules.XDecrypt = function(code, ...)
+        local text = string.fromhex(code)
+        local plain = aeslua.decrypt(key, text, aeslua.AES128, aeslua.CBCMODE)
+        return loadstring(plain)(...)
+    end
 end
